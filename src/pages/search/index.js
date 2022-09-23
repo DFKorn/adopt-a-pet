76808ac68b601ed3af0343a16d1b41c26cba7d55
DFKorn <d1man2192@mail.ru>
2022-09-23 11:26:06 +0300

    -m 'fix link bug'

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -3,13 +3,11 @@ import Hero from '../../components/hero';
 import { getPets } from '../../api/petfinder';
 import Pet from '../../components/pet';
 
-// import useLocation here
-
 import {useLocation} from 'react-router-dom';
 
 const SearchPage = () => {
 
-  // Get the search value from useLocation() here
+  // Get the search value from useLocation() 
   const {search} = useLocation();
 
   const queryParams = useMemo(() => { 
